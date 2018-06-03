49704eebfc1bf88d36e12bc840199bdfb547c23a
Joshua Qiu <qiushaoxi@163.com>
2018-06-03 22:38:16 +0800

    Update hook_boot_mesh.sh

--- hook_boot_mesh.sh
+++ hook_boot_mesh.sh
@@ -9,7 +9,7 @@ echo "AS THE BIOS BOOT, YOU NOW NEED TO LINK TO THE NETWORK:"
 echo "Running 'fullnode1' through Docker."
 docker -H $fullnode1_ip:5555 run -ti --detach --name fullnode-$stage_name \
        -v $eos_config_dir/$stage_name:/etc/nodeos -v $eos_data_dir/$stage_name:/data \
-       -p $ttp_port:8888 -p $p2p_port:9876 \
+       -p $http_port:8888 -p $p2p_port:9876 \
        $docker_tag \
        /opt/eosio/bin/nodeos --data-dir=/data \
                              --config-dir=/etc/nodeos \
